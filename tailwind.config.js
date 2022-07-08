347247671c0383c2ff01916a72ab8280ded6aa30
RobiNN <kelcakrobo@gmail.com>
2022-07-09 00:06:07 +0200

    Update TailwindCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,25 +7,25 @@ module.exports = {
     },
     safelist: [
         {
-            pattern: /bg-(slate|red|emerald|sky)-(50|100|500)/,
+            pattern: /^bg-(slate|red|emerald|sky)-(50|100|500)?$/,
         },
         {
-            pattern: /bg-(slate|red|emerald|sky)-600/,
+            pattern: /^bg-(slate|red|emerald|sky)-600?$/,
             variants: ['hover']
         },
         {
-            pattern: /text-(slate|red|emerald|sky)-(500|900)/,
+            pattern: /^text-(slate|red|emerald|sky)-(500|900)?$/,
         },
         {
-            pattern: /text-(slate|red|emerald|sky)-700/,
+            pattern: /^text-(slate|red|emerald|sky)-700?$/,
             variants: ['hover']
         },
         {
-            pattern: /border-(slate|red|emerald|sky)-300/,
+            pattern: /^border-(slate|red|emerald|sky)-300?$/,
             variants: ['focus']
         },
         {
-            pattern: /ring-(slate|red|emerald|sky)-(100|200|300)/,
+            pattern: /^ring-(slate|red|emerald|sky)-(100|200|300)?$/,
             variants: ['focus']
         },
         'ml-64',

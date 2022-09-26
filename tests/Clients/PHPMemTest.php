d230eb35776b02f958f6e65f82ed000818904e03
RobiNN <kelcakrobo@gmail.com>
2022-09-26 16:23:40 +0200

    Update tests

---
+++ tests/Clients/PHPMemTest.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * This file is part of phpCacheAdmin.
+ *
+ * Copyright (c) Róbert Kelčák (https://kelcak.com/)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Tests\Clients;
+
+use RobiNN\Pca\Dashboards\Memcached\Compatibility\PHPMem;
+use RobiNN\Pca\Dashboards\Memcached\MemcachedException;
+use Tests\TestCase;
+
+final class PHPMemTest extends TestCase {
+    private PHPMem $phpmem;
+
+    protected function setUp(): void {
+        $server = ['host' => '127.0.0.1'];
+        $this->phpmem = new PHPMem($server);
+        $this->phpmem->addServer($server['host']);
+    }
+
+    /**
+     * @throws MemcachedException
+     */
+    public function testIsConnected(): void {
+        $this->assertTrue($this->phpmem->isConnected());
+    }
+
+    /**
+     * @throws MemcachedException
+     */
+    public function testSetGetKey(): void {
+        $keys = [
+            'string' => ['original' => 'phpCacheAdmin', 'expected' => 'phpCacheAdmin'],
+            'int'    => ['original' => 23, 'expected' => '23'],
+            'float'  => ['original' => 23.99, 'expected' => '23.99'],
+            'bool'   => ['original' => true, 'expected' => '1'],
+            'null'   => ['original' => null, 'expected' => ''],
+            'array'  => [
+                'original' => ['key1', 'key2'],
+                'expected' => 'a:2:{i:0;s:4:"key1";i:1;s:4:"key2";}',
+            ],
+            'object' => [
+                'original' => (object) ['key1', 'key2'],
+                'expected' => 'O:8:"stdClass":2:{s:1:"0";s:4:"key1";s:1:"1";s:4:"key2";}',
+            ],
+        ];
+
+        foreach ($keys as $key => $value) {
+            $this->phpmem->set('pu-pmem-test-'.$key, $value['original']);
+        }
+
+        $this->assertSame($keys['string']['expected'], $this->phpmem->getKey('pu-pmem-test-string'));
+        $this->assertSame($keys['int']['expected'], $this->phpmem->getKey('pu-pmem-test-int'));
+        $this->assertSame($keys['float']['expected'], $this->phpmem->getKey('pu-pmem-test-float'));
+        $this->assertSame($keys['bool']['expected'], $this->phpmem->getKey('pu-pmem-test-bool'));
+        $this->assertSame($keys['null']['expected'], $this->phpmem->getKey('pu-pmem-test-null'));
+        $this->assertSame($keys['array']['expected'], $this->phpmem->getKey('pu-pmem-test-array'));
+        $this->assertSame($keys['object']['expected'], $this->phpmem->getKey('pu-pmem-test-object'));
+
+        foreach ($keys as $key => $value) {
+            $this->phpmem->delete('pu-pmem-test-'.$key);
+        }
+    }
+
+    /**
+     * @throws MemcachedException
+     */
+    public function testDeleteKey(): void {
+        $key = 'pu-pmem-test-delete-key';
+
+        $this->phpmem->set($key, 'data');
+
+        $this->assertTrue($this->phpmem->delete($key));
+    }
+
+    /**
+     * @throws MemcachedException
+     */
+    public function testGetServerStats(): void {
+        $stats = $this->phpmem->getServerStats();
+
+        $this->assertIsArray($stats);
+        $this->assertArrayHasKey('version', $stats);
+    }
+}
---
+++ tests/Clients/PredisTest.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * This file is part of phpCacheAdmin.
+ *
+ * Copyright (c) Róbert Kelčák (https://kelcak.com/)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Tests\Clients;
+
+use RobiNN\Pca\Dashboards\DashboardException;
+use RobiNN\Pca\Dashboards\Redis\Compatibility\Predis;
+use Tests\TestCase;
+
+final class PredisTest extends TestCase {
+    private Predis $predis;
+
+    protected function setUp(): void {
+        $this->predis = new Predis(['host' => '127.0.0.1']);
+        $this->predis->connect();
+    }
+
+    /**
+     * @throws DashboardException
+     */
+    public function testGetType(): void {
+        $this->predis->set('pu-pred-test-string', 'value');
+        $this->predis->sadd('pu-pred-test-set', 'value1', 'value2', 'value3');
+        $this->predis->rpush('pu-pred-test-list', 'value1', 'value2', 'value3');
+        $this->predis->zadd('pu-pred-test-zset', 0, 'value1', 1, 'value2', 2, 'value3');
+        $this->predis->hset('pu-pred-test-hash', 'hashkey1', 'value1');
+        $this->predis->hset('pu-pred-test-hash', 'hashkey2', 'value2');
+        $this->predis->xAdd('pu-pred-test-stream', '*', ['field1' => 'value1', 'field2' => 'value2']);
+        $this->predis->xAdd('pu-pred-test-stream', '*', ['field3' => 'value3']);
+
+        $this->assertSame('string', $this->predis->getType('pu-pred-test-string'));
+        $this->assertSame('set', $this->predis->getType('pu-pred-test-set'));
+        $this->assertSame('list', $this->predis->getType('pu-pred-test-list'));
+        $this->assertSame('zset', $this->predis->getType('pu-pred-test-zset'));
+        $this->assertSame('hash', $this->predis->getType('pu-pred-test-hash'));
+        $this->assertSame('stream', $this->predis->getType('pu-pred-test-stream'));
+    }
+
+    public function testDelete(): void {
+        $this->predis->del('pu-pred-test-string');
+        $this->predis->del('pu-pred-test-set');
+        $this->predis->del('pu-pred-test-list');
+        $this->predis->del('pu-pred-test-zset');
+        $this->predis->del('pu-pred-test-hash');
+        $this->predis->del('pu-pred-test-stream');
+
+        $this->assertSame(0, $this->predis->exists('pu-pred-test-string'));
+        $this->assertSame(0, $this->predis->exists('pu-pred-test-set'));
+        $this->assertSame(0, $this->predis->exists('pu-pred-test-list'));
+        $this->assertSame(0, $this->predis->exists('pu-pred-test-zset'));
+        $this->assertSame(0, $this->predis->exists('pu-pred-test-hash'));
+        $this->assertSame(0, $this->predis->exists('pu-pred-test-stream'));
+    }
+}

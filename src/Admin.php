d77364e1166ab79282dbfacaf984104afbd76e93
RobiNN <kelcakrobo@gmail.com>
2022-09-26 22:27:49 +0200

    v1.3.0

--- src/Admin.php
+++ src/Admin.php
@@ -18,7 +18,7 @@ class Admin {
     /**
      * @const string phpCacheAdmin version.
      */
-    public const VERSION = '1.2.0';
+    public const VERSION = '1.3.0';
 
     /**
      * @var array<string, DashboardInterface>

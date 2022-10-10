51ed7b63d6919c4186a93dc0b21c421b3399c7e9
Róbert Kelčák <kelcakrobo@gmail.com>
2022-10-10 19:15:29 +0200

    Update Admin.php

--- src/Admin.php
+++ src/Admin.php
@@ -18,7 +18,7 @@ class Admin {
     /**
      * @const string phpCacheAdmin version.
      */
-    public const VERSION = '1.3.0';
+    public const VERSION = '1.3.1';
 
     /**
      * @var array<string, DashboardInterface>

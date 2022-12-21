36c9e7b50015228f129cfc06e16571f3ec01a5b6
Róbert Kelčák <kelcakrobo@gmail.com>
2022-12-22 00:14:39 +0100

    Update CommandTrait.php

--- src/Dashboards/Memcached/Compatibility/CommandTrait.php
+++ src/Dashboards/Memcached/Compatibility/CommandTrait.php
@@ -16,7 +16,37 @@ use RobiNN\Pca\Dashboards\Memcached\MemcachedException;
 
 trait CommandTrait {
     /**
-     * Run Memcached command.
+     * Unknown or incorrect commands can cause an infinite loop,
+     * so only tested commands are allowed.
+     *
+     * @var array<int, string>
+     */
+    private array $allowed_commands = [
+        'set', 'add', 'replace', 'append', 'prepend', 'cas', 'get', 'gets', 'gat', 'gats',
+        'touch', 'delete', 'incr', 'decr', 'stats', 'flush_all', 'version', 'lru_crawler',
+    ];
+
+    /**
+     * Commands without a specific end string.
+     *
+     * @var array<int, string>
+     */
+    private array $no_end = [
+        'incr', 'decr',
+    ];
+
+    /**
+     * Loop until the server returns one of these strings.
+     *
+     * @var array<int, string>
+     */
+    private array $with_end = [
+        'ERROR', 'CLIENT_ERROR', 'SERVER_ERROR', 'STORED', 'NOT_STORED', 'EXISTS', 'NOT_FOUND', 'TOUCHED', 'DELETED',
+        'OK', 'END', 'VERSION', 'BUSY', 'BADCLASS', 'NOSPARE', 'NOTFULL', 'UNSAFE', 'SAME', 'RESET',
+    ];
+
+    /**
+     * Run command.
      *
      * set|add|replace|append|prepend <key> <flags> <ttl> <bytes>\r\n<value>
      * cas <key> <flags> <ttl> <bytes> <cas unique>\r\n<value>
@@ -34,7 +64,8 @@ trait CommandTrait {
      * lru_crawler crawl <...classid|all>
      * lru_crawler metadump <...classid|all|hash>
      *
-     * Note: \r\n is added automatically to the end.
+     * Note: \r\n is added automatically to the end
+     * and \r\n (as a plain string) is converted to a real end of line.
      *
      * @link https://github.com/memcached/memcached/blob/master/doc/protocol.txt
      *
@@ -50,22 +81,12 @@ trait CommandTrait {
         while (!feof($fp)) {
             $buffer .= fgets($fp, 256);
 
-            // These do not have a specific end string
-            $no_end = [
-                'incr', 'decr',
-            ];
-
             // Loop only once
-            if (in_array($this->commandName($command), $no_end)) {
+            /*if (in_array($this->commandName($command), $this->no_end, true)) {
                 break;
-            }
+            }*/
 
-            $ends = [
-                'ERROR', 'CLIENT_ERROR', 'SERVER_ERROR', 'STORED', 'NOT_STORED', 'EXISTS', 'NOT_FOUND', 'TOUCHED', 'DELETED',
-                'OK', 'END', 'VERSION', 'BUSY', 'BADCLASS', 'NOSPARE', 'NOTFULL', 'UNSAFE', 'SAME', 'RESET',
-            ];
-
-            foreach ($ends as $end) {
+            foreach ($this->with_end as $end) {
                 if (preg_match('/^'.$end.'/imu', $buffer)) {
                     break 2;
                 }
@@ -98,7 +119,7 @@ trait CommandTrait {
      * @throws MemcachedException
      */
     private function resource(string $command) {
-        if ($this->checkCommand($command) === false) {
+        if (!in_array($this->commandName($command), $this->allowed_commands, true)) {
             throw new MemcachedException('Unknown or incorrect command "'.$command.'".');
         }
 
@@ -127,16 +148,5 @@ trait CommandTrait {
         $parts = explode(' ', $command);
 
         return strtolower($parts[0]);
-    }
-
-    private function checkCommand(string $command): bool {
-        // Unknown or incorrect commands can cause an infinite loop,
-        // so only tested commands are allowed
-        $allowed = [
-            'set', 'add', 'replace', 'append', 'prepend', 'cas', 'get', 'gets', 'gat', 'gats',
-            'touch', 'delete', 'incr', 'decr', 'stats', 'flush_all', 'version', 'lru_crawler',
-        ];
-
-        return in_array($this->commandName($command), $allowed, true);
     }
 }

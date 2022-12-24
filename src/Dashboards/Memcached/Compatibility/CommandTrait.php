c10eb827d92691860e081d80b04f90607f448f36
Róbert Kelčák <kelcakrobo@gmail.com>
2022-12-24 01:58:08 +0100

    Update CommandTrait.php

--- src/Dashboards/Memcached/Compatibility/CommandTrait.php
+++ src/Dashboards/Memcached/Compatibility/CommandTrait.php
@@ -82,12 +82,12 @@ trait CommandTrait {
      * @throws MemcachedException
      */
     public function runCommand(string $command, bool $array = false) {
-        $fp = $this->resource($command);
+        $stream = $this->createSocket($command);
         $buffer = '';
         $data = [];
 
-        while (!feof($fp)) {
-            $buffer .= fgets($fp, 256);
+        while (!feof($stream)) {
+            $buffer .= fgets($stream, 256);
 
             // Loop only once
             if (in_array($this->commandName($command), $this->no_end, true)) {
@@ -111,7 +111,7 @@ trait CommandTrait {
             }
         }
 
-        fclose($fp);
+        fclose($stream);
 
         if ($array === true) {
             return $data;
@@ -125,29 +125,28 @@ trait CommandTrait {
      *
      * @throws MemcachedException
      */
-    private function resource(string $command) {
+    private function createSocket(string $command) {
         if (!in_array($this->commandName($command), $this->allowed_commands, true)) {
             throw new MemcachedException('Unknown or incorrect command "'.$command.'".');
         }
 
         if (isset($this->server['path'])) {
-            $fp = @stream_socket_client('unix://'.$this->server['path'], $error_code, $error_message);
+            $stream = @stream_socket_client('unix://'.$this->server['path'], $error_code, $error_message);
         } else {
-            $fp = @fsockopen($this->server['host'], (int) $this->server['port'], $error_code, $error_message, 3);
+            $stream = @fsockopen($this->server['host'], (int) $this->server['port'], $error_code, $error_message, 3);
         }
 
-        if ($error_message !== '' || $fp === false) {
+        if ($error_message !== '' || $stream === false) {
             throw new MemcachedException('Command: "'.$command.'": '.$error_message);
         }
 
-        $command = strtr($command, ['\r\n' => "\r\n"]);
-        $write = fwrite($fp, $command."\r\n");
+        $command = strtr($command, ['\r\n' => "\r\n"])."\r\n";
 
-        if ($write === false) {
+        if (fwrite($stream, $command, strlen($command)) === false) {
             throw new MemcachedException('Command: "'.$command.'": Not valid resource.');
         }
 
-        return $fp;
+        return $stream;
     }
 
     private function commandName(string $command): string {

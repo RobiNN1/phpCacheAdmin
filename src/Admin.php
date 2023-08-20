49f0174d8b6cde9399825287d75f337ce76d2332
Róbert Kelčák <kelcakrobo@gmail.com>
2023-08-20 18:25:16 +0200

    Update Admin.php

--- src/Admin.php
+++ src/Admin.php
@@ -15,7 +15,7 @@ namespace RobiNN\Pca;
 use RobiNN\Pca\Dashboards\DashboardInterface;
 
 class Admin {
-    public const VERSION = '1.5.4';
+    public const VERSION = '1.6.0';
 
     /**
      * @var array<string, DashboardInterface>

f04ff6777ef5a7af3b9a96688eacba6e00b21c67
RobiNN1 <RobiNN1@users.noreply.github.com>
2025-02-16 11:00:18 +0100

    Update Admin.php

--- src/Admin.php
+++ src/Admin.php
@@ -11,7 +11,7 @@ namespace RobiNN\Pca;
 use RobiNN\Pca\Dashboards\DashboardInterface;
 
 class Admin {
-    public const VERSION = '2.1.0';
+    public const VERSION = '2.1.1';
 
     /**
      * @var array<string, DashboardInterface>

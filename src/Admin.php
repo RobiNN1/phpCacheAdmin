70f3f4a4ca852330ebdf0e1c8887cd6f244f19f0
RobiNN1 <RobiNN1@users.noreply.github.com>
2026-04-06 00:54:14 +0200

    Update predis & twig

--- src/Admin.php
+++ src/Admin.php
@@ -11,7 +11,7 @@ namespace RobiNN\Pca;
 use RobiNN\Pca\Dashboards\DashboardInterface;
 
 class Admin {
-    public const VERSION = '2.5.0';
+    public const VERSION = '2.5.1';
 
     private readonly Template $template;
 

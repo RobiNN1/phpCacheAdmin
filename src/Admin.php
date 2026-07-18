7215535f33998561bb336398efc1e8eb9a0f3dc1
RobiNN1 <RobiNN1@users.noreply.github.com>
2026-07-18 12:10:15 +0200

    Update Admin.php

--- src/Admin.php
+++ src/Admin.php
@@ -11,7 +11,7 @@ namespace RobiNN\Pca;
 use RobiNN\Pca\Dashboards\DashboardInterface;
 
 class Admin {
-    public const VERSION = '2.6.1';
+    public const VERSION = '2.7.0';
 
     private readonly Template $template;
 
